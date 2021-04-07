ee6fc7d7a40de2cfa503d8f5ec653ea4b41ee4af
KyomiTeam <77785840+KyomiTeam@users.noreply.github.com>
2021-04-07 12:12:45 +0200

    Update v2.0

--- Jeu De Cul/app/src/main/java/fr/app/kyomi/GameC.java
+++ Jeu De Cul/app/src/main/java/fr/app/kyomi/GameC.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.AttributedString;
 
 public class GameC extends AppCompatActivity {
 
     private TextView txt1;
+    private TextView txtAct;
     private Button play;
+    private FloatingActionButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +32,20 @@ public class GameC extends AppCompatActivity {
 
 
         // Game
+        String player1 = MainActivity.p1.getText().toString();
+        System.out.println("Player 1: " + player1);
+        String player2 = MainActivity.p2.getText().toString();
+        System.out.println("Player 2: " + player2);
+
         this.txt1 = findViewById(R.id.textView1);
+        this.txtAct = findViewById(R.id.textAction);
 
         String[]  smileyOne = {"\uD83D\uDE1B", "\uD83D\uDC46", "\uD83D\uDC4B", "\uD83D\uDC45", "\uD83D\uDC40", "\uD83D\uDC8B"};
+        String[]  action = {"toucher", "regarder", "embrasser", "lécher", "sucer", "claquer"};
         String[] smileyTwo = {"\uD83E\uDD50", "\uD83C\uDF2E", "\uD83C\uDF69", "\uD83C\uDF46", "\uD83C\uDF52", "\uD83C\uDF51"};
+        String[]  object = {"lèvres", "fesses", "seins", "couilles", "bite", "chatte"};
+
+        final int[] forPlayer1 = {0};
 
         this.play = findViewById(R.id.btnPlay);
         play.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +56,33 @@ public class GameC extends AppCompatActivity {
                 int rdn2;
                 rdn2 = (int)(Math.random() * 6);
 
-                System.out.println(rdn1);
+                System.out.println("First Random: " + rdn1 + ", Second: " + rdn2);
 
-                txt1.setTextSize(35);
+                txt1.setTextSize(40);
                 txt1.setText(smileyOne[rdn1] + "  ➡️  " + smileyTwo[rdn2]);
+
+                txtAct.setTextSize(17);
+                if (forPlayer1[0] == 0 ){
+                    //txtAct.setText(player1 + " doit " + action[rdn1] + " le(s)/la " + object[rdn2] + " de " + player2);
+                    txtAct.setText(Html.fromHtml("<b>"+player1+"</b> doit <u>" + action[rdn1] + "\n</u> le(s)/la <u>" + object[rdn2] + "</u> de <b>" + player2+"</b>"));
+                    forPlayer1[0] = 1;
+                } else {
+                    //txtAct.setText(player2 + " doit " + action[rdn1] + " le(s)/la " + object[rdn2] + " de " + player1);
+                    txtAct.setText(Html.fromHtml("<b>"+player2+"</b> doit <u>" + action[rdn1] + "\n</u> le(s)/la <u>" + object[rdn2] + "</u> de <b>" + player1+"</b>"));
+                    forPlayer1[0] = 0;
+                }
             }
         });
 
-
+        this.back = findViewById(R.id.floatingActionButton2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondAct = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(secondAct);
+                finish();
+            }
+        });
 
     }
 
--- Jeu De Cul/app/src/main/java/fr/app/kyomi/MainActivity.java
+++ Jeu De Cul/app/src/main/java/fr/app/kyomi/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,6 +17,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button enter;
+    private Button infos;
+    public static EditText p1;
+    public static EditText p2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +29,28 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Jeu de Cul bien lancée...", Toast.LENGTH_SHORT).show();
 
         this.enter = findViewById(R.id.btnEnter);
+        this.p1 = findViewById(R.id.p1);
+        this.p2 = findViewById(R.id.p2);
 
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent secondAct = new Intent(getApplicationContext(), GameC.class);
                 startActivity(secondAct);
                 finish();
             }
         });
+
+        this.infos = findViewById(R.id.btnInfos);
+
+        infos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Indisponible pour le moment.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
